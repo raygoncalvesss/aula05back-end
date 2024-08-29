d7c487acd496a77c6b5063fe4919c40356e56f97
Rayssa <rayssagoncalves.tds2.senai@gmail.com>
2024-08-29 11:53:47 -0300

    refactor: start to refactor api routes

---
+++ src/routes/emocoes.routes.js
@@ -0,0 +1,22 @@
+import { Router } from "express";
+
+const emocoesRouter = Router()
+const emocoes = [
+    {
+        id: 1,
+        nome: "Alegria",
+        cor: "Amarela"
+    },
+
+    {
+        id: 2,
+        nome: "Nojinho",
+        cor: "Verde"
+    },
+
+    {
+        id: 3,
+        nome: "Ansiedade",
+        cor: "Laranja"
+    },
+]
---
+++ src/routes/personagens.routes.js
@@ -0,0 +1,27 @@
+import { Router } from "express";
+
+const personagensRouter = Router()
+const personagens = [
+    {
+        id: 100,
+        nome: "pateta",
+        studio: "disney",
+        vivo: true
+    },
+
+    {
+        id: 101,
+        nome: "bob esponja",
+        studio: "nickelodeon",
+        vivo: true
+    },
+
+    {
+        id: 102,
+        nome: "patrick",
+        studio: "nickelodeon",
+        vivo: true
+    },
+
+
+]
