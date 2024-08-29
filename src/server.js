f13fba12a51f9b7928e9b06295de63990a79a01a
Rayssa <rayssagoncalves.tds2.senai@gmail.com>
2024-08-29 10:53:47 -0300

    feat: add some requests

--- src/server.js
+++ src/server.js
@@ -8,12 +8,65 @@ const serverPort = process.env.PORT || 3000
 const app = express()
 app.use(express.json())
 
+const emocoes = [
+    {
+        id: 1,
+        nome: "Alegria",
+        cor: "Amarela"
+    },
+
+    {
+        id: 2,
+        nome: "Nojinho",
+        cor: "Verde"
+    },
+
+    {
+        id: 3,
+        nome: "Ansiedade",
+        cor: "Laranja"
+    },
+]
+
+const personagens = [
+    {
+        id: 100,
+        nome: "pateta",
+        studio: "disney",
+        vivo: true
+    },
+
+    {
+        id: 101,
+        nome: "bob esponja",
+        studio: "nickelodeon",
+        vivo: true
+    },
+
+    {
+        id: 102,
+        nome: "patrick",
+        studio: "nickelodeon",
+        vivo: true
+    },
+
+
+]
+
 app.get("/", (req, res) => {
     return res.status(200).send({ message: "Hello, World" })
 })
 
 app.get("/2tds2", (req, res) => {
     return res.status(200).send({ message: "Hello, World" })
+})
+
+app.get("/emocoes", (req, res) => {
+    return res.status(200).send( emocoes )
+})
+
+app.get("/personagens", (req, res) => {
+    return res.status(200).send( personagens )
 })
 
 app.listen(serverPort, () => {
