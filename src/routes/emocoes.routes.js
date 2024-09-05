4edd5d802318ca832c1620fd00482e96714c78a3
Rayssa <rayssagoncalves.tds2.senai@gmail.com>
2024-09-05 17:20:38 -0300

    feat: create get by id method

--- src/routes/emocoes.routes.js
+++ src/routes/emocoes.routes.js
@@ -20,20 +20,38 @@ const emocoes = [
         cor: "Laranja"
     },
 ]
-
+//rota para buscar todas as emoções
 emocoesRouter.get("/", (req,res) => {
     return res.status(200).send(emocoes)
 });
 
+//rota para criar nova emoção
 emocoesRouter.post("/", (req,res) => {
     const {nome, cor} = req.body
-    const newEmotion = {
+    const newemocao = {
         id: emocoes.length + 1,
         nome: nome,
         cor: cor
     }
     emocoes.push(newemocao)
-    return res.status(200).send(emocoes)
+    return res.status(201).send(emocoes)
+});
+
+emocoesRouter.get("/:id", (req, res) => {
+    const { id } = req.params;
+
+    console.log(id);
+    const emocao = emocoes.find( (emotion) => emotion.id == id )
+
+    if (!emocao) {
+        return res.status(404).send({
+            message: "emoção não encontrada!",
+        });
+    }
+
+    return res.status(200).send({
+        message: "emoção encontrada", emocao,
+    })
 });
 
 export default emocoesRouter
